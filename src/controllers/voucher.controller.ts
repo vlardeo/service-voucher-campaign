23f8874e1c99f7013e77dc96c1f19b9a38680149
Vladislav Bobrovnikov <bobrovnikov.v.i@gmail.com>
2023-03-06 18:43:24 +0400

    feat: voucherController.downloadVouchersAsCsv

--- src/controllers/voucher.controller.ts
+++ src/controllers/voucher.controller.ts
@@ -1,8 +1,13 @@
 import type { Request, Response, NextFunction } from 'express';
 import voucherService from '@/services/voucher.service';
+import Papa from 'papaparse';
+import campaignService from '@/services/campaign.service';
 
 export type VoucherBatchCreateRequest = Request<{ campaignId: string }, null, null, { amount: string }>;
 export type VoucherListPerCampaignRequest = Request<{ campaignId: string }, null, null, { page?: string; pageSize?: string }>;
+export type DownloadVouchersAsCsv = Request<{ campaignId: string }>;
+
+const EXPORT_CSV_PAGE_SIZE = 500;
 
 const voucherController = {
   voucherBatchCreate: async (req: VoucherBatchCreateRequest, res: Response, next: NextFunction) => {
@@ -30,6 +35,44 @@ const voucherController = {
       });
 
       res.set('X-Total-Count', String(total)).status(200).json(results);
+    } catch (err: any) {
+      next(err);
+    }
+  },
+
+  downloadVouchersAsCsv: async (req: DownloadVouchersAsCsv, res: Response, next: NextFunction) => {
+    const { campaignId } = req.params;
+
+    try {
+      const campaign = await campaignService.findById(campaignId);
+
+      if (!campaign) {
+        res.status(404).send({ error: `Campaign ${campaignId} doesn't exist` });
+      }
+
+      let page = 0;
+      let { results } = await voucherService.listVouchersPerCampaign({ campaignId, page, pageSize: EXPORT_CSV_PAGE_SIZE });
+
+      if (!results.length) {
+        res.status(404).send({ error: `Vouchers for campaign ${campaignId} don't exist` });
+      }
+
+      let csv = '';
+
+      res.set('Content-Type', 'text/csv');
+      res.set('Content-Disposition', `attachment; filename=vouchers-${campaignId}.csv`);
+
+      // While there are vouchers, we convert it to the CSV, stream to the client and get another page of vouchers
+      while (results.length) {
+        page++;
+
+        csv = Papa.unparse(results);
+        res.write(csv);
+
+        ({ results } = await voucherService.listVouchersPerCampaign({ campaignId, page, pageSize: EXPORT_CSV_PAGE_SIZE }));
+      }
+
+      res.status(200).end();
     } catch (err: any) {
       next(err);
     }
--- tests/unit/controllers/voucher-controller.spec.ts
+++ tests/unit/controllers/voucher-controller.spec.ts
@@ -1,6 +1,9 @@
 import aVoucherService from '@tests/mocks/voucher.service';
 jest.mock('@/services/voucher.service', () => aVoucherService);
 
+import aCampaignService from '@tests/mocks/campaign.service';
+jest.mock('@/services/campaign.service', () => aCampaignService);
+
 import { aCampaign } from '@tests/builders/campaign.builder';
 import { mockNext, mockResponse } from '@tests/mocks/express-api';
 import { generateUuid } from '@tests/uuid';
@@ -57,6 +60,98 @@ describe('@controllers/voucher-controller', () => {
       expect(mockResponse.set).toHaveBeenCalledWith('X-Total-Count', '2');
       expect(mockResponse.status).toHaveBeenCalledWith(200);
       expect(mockResponse.json).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('downloadVouchersAsCsv()', () => {
+    let req: VoucherListPerCampaignRequest;
+
+    beforeEach(() => {
+      req = { query: {}, params: {} } as VoucherListPerCampaignRequest;
+    });
+
+    describe('when there is no campaign', () => {
+      it('should return response with status code 404', async () => {
+        (aCampaignService.findById as jest.Mock).mockResolvedValueOnce(null);
+        await voucherController.downloadVouchersAsCsv(req, mockResponse, mockNext);
+        expect(mockResponse.status).toHaveBeenCalledWith(404);
+      });
+    });
+
+    describe('when there are no vouchers for campaign', () => {
+      it('should return response with status code 404', async () => {
+        const campaign = aCampaign({}).buildMock();
+        (aCampaignService.findById as jest.Mock).mockResolvedValueOnce(campaign);
+        (aVoucherService.listVouchersPerCampaign as jest.Mock).mockResolvedValueOnce({
+          results: [],
+          total: 0,
+        });
+        await voucherController.downloadVouchersAsCsv(req, mockResponse, mockNext);
+        expect(mockResponse.status).toHaveBeenCalledWith(404);
+      });
+    });
+
+    describe('when there are voucher', () => {
+      describe('when vouchers total count is less than export csv page size', () => {
+        it('should send file at once', async () => {
+          const campaign = aCampaign({}).buildMock();
+          const vouchers = [aVoucher({ campaignId: campaign.id }).buildMock(), aVoucher({ campaignId: campaign.id }).buildMock()];
+
+          (aCampaignService.findById as jest.Mock).mockResolvedValueOnce(campaign);
+          (aVoucherService.listVouchersPerCampaign as jest.Mock)
+            .mockResolvedValueOnce({
+              results: vouchers,
+              total: 2,
+            })
+            .mockResolvedValueOnce({
+              results: [],
+              total: 0,
+            });
+
+          req.params = { campaignId: campaign.id };
+
+          await voucherController.downloadVouchersAsCsv(req, mockResponse, mockNext);
+
+          expect(mockResponse.set).toHaveBeenCalledWith('Content-Type', 'text/csv');
+          expect(mockResponse.set).toHaveBeenCalledWith('Content-Disposition', `attachment; filename=vouchers-${campaign.id}.csv`);
+          expect(mockResponse.write).toHaveBeenCalledTimes(1);
+          expect(mockResponse.status).toHaveBeenCalledWith(200);
+          expect(mockResponse.end).toHaveBeenCalledTimes(1);
+        });
+      });
+
+      describe('when vouchers total count is more than export csv page size', () => {
+        it('should write response several times and stream file partially to the client', async () => {
+          const campaign = aCampaign({}).buildMock();
+          const vouchersPage1 = [aVoucher({ campaignId: campaign.id }).buildMock(), aVoucher({ campaignId: campaign.id }).buildMock()];
+          const vouchersPage2 = [aVoucher({ campaignId: campaign.id }).buildMock(), aVoucher({ campaignId: campaign.id }).buildMock()];
+
+          (aCampaignService.findById as jest.Mock).mockResolvedValueOnce(campaign);
+          (aVoucherService.listVouchersPerCampaign as jest.Mock)
+            .mockResolvedValueOnce({
+              results: vouchersPage1,
+              total: 2,
+            })
+            .mockResolvedValueOnce({
+              results: vouchersPage2,
+              total: 2,
+            })
+            .mockResolvedValueOnce({
+              results: [],
+              total: 0,
+            });
+
+          req.params = { campaignId: campaign.id };
+
+          await voucherController.downloadVouchersAsCsv(req, mockResponse, mockNext);
+
+          expect(mockResponse.set).toHaveBeenCalledWith('Content-Type', 'text/csv');
+          expect(mockResponse.set).toHaveBeenCalledWith('Content-Disposition', `attachment; filename=vouchers-${campaign.id}.csv`);
+          expect(mockResponse.write).toHaveBeenCalledTimes(2);
+          expect(mockResponse.status).toHaveBeenCalledWith(200);
+          expect(mockResponse.end).toHaveBeenCalledTimes(1);
+        });
+      });
     });
   });
 });
